0c497af323adaa3d2d8a564ef93e644d92067575
Wang Ke <947789088@qq.com>
2024-01-06 01:54:00 +0800

    新增了悬浮按钮，为悬浮按钮编写了点击事件

--- app/src/main/java/com/example/ditingsmarttravel/TripFragment.kt
+++ app/src/main/java/com/example/ditingsmarttravel/TripFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TripFragment : Fragment() {
 
@@ -15,5 +17,14 @@ class TripFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trip, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fabTrip = view.findViewById<FloatingActionButton>(R.id.fab_trip)
+        fabTrip.setOnClickListener {
+            Toast.makeText(activity, "你点击了新建按钮", Toast.LENGTH_SHORT).show()
+        }
     }
 }
